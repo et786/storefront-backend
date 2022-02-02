ca42e7b627735a4c1a24832f0e8a5233a02e92f3
ahmed-t-syed <Ahmed>
2022-02-01 22:45:27 -0700

    lesson1 has no exercises, so it is empty

---
+++ sandbox.sql
@@ -0,0 +1,16 @@
+CREATE TABLE places (
+    
+    id INT(7) NOT NULL AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR NOT NULL,
+    location VARCHAR NOT NULL
+
+);
+
+
+CREATE TABLE people (
+    
+    id INT(7) NOT NULL AUTO_INCREMENT PRIMARY KEY
+    name VARCHAR NOT NULL,
+    location VARCHAR NOT NULL
+
+);
