0968d4f47d4cad9c9466d8df64cb7c834a3f4ba3
ahmed-t-syed <Ahmed>
2022-02-07 02:44:46 -0700

    yarn watch script now working

--- dist/server.js
+++ dist/server.js
@@ -2,16 +2,17 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
-var app = (0, express_1.default)();
-var address = "0.0.0.0:3000";
-app.use(body_parser_1.default.json());
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+var app = (0, express_1["default"])();
+var port = 7861;
+var address = "0.0.0.0:".concat(port);
+app.use(body_parser_1["default"].json());
+app.get("/", function (req, res) {
+    res.send("/");
 });
-app.listen(3000, function () {
-    console.log("starting app on: ".concat(address));
+app.listen(port, function () {
+    console.log("Server listening on ".concat(address, "..."));
 });
-exports.default = app;
+exports["default"] = app;
