13f4ffb4e61e4deb772a96e2d0c6ce4c67cab450
ahmed-t-syed <Ahmed>
2022-02-07 01:15:52 -0700

    Spec text duplication issue resolved

--- dist/tests/indexSpec.js
+++ dist/tests/indexSpec.js
@@ -1,1 +1,11 @@
 "use strict";
+describe("placeholder", function () {
+    it("expect to be shown in console", function () {
+        // trivial test
+        expect(1).toBe(1);
+    });
+});
+describe("not placeholder", function () {
+    it("expect to be shown", function () {
+    });
+});
---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,12 @@
+describe("placeholder", () => {
+    it("expect to be shown in console", () => {
+        // trivial test
+        expect(1).toBe(1)
+    })
+})
+
+describe("not placeholder", () => {
+    it("expect to be shown", () =>{
+
+    })
+})
